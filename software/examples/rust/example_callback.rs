6a5ef4bfaad6384287972c5ae5c8976c5a0c2fa7
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:29:26 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for pressure events.
-    let pressure_receiver = p.get_pressure_receiver();
+    let pressure_receiver = p.get_pressure_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `p` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `p` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for pressure in pressure_receiver {
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get threshold receivers with a debounce time of 10 seconds (10000ms).
     p.set_debounce_period(10000);
 
-    // Create receiver for pressure reached events.
-    let pressure_reached_receiver = p.get_pressure_reached_receiver();
+    let pressure_reached_receiver = p.get_pressure_reached_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `p` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `p` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for pressure_reached in pressure_reached_receiver {
