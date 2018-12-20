cefdb3423db2b43e8381258436a3030835c4f2a4
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:58:02 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  pressure_bricklet::*};
-
+use tinkerforge::{ip_connection::IpConnection, pressure_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let p = PressureBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current pressure.
-let pressure = p.get_pressure().recv()?;
-		println!("Pressure: {} kPa", pressure as f32 /1000.0);
+    // Get current pressure.
+    let pressure = p.get_pressure().recv()?;
+    println!("Pressure: {} kPa", pressure as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
