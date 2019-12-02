49b54f042fd2eee2bc753e92161ef90b52fefdba
Matthias Bolte <matthias@tinkerforge.com>
2019-12-02 18:25:16 +0100

    Remove examples

--- software/examples/shell/example-simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Connects to localhost:4223 by default, use --host and --port to change this
-
-uid=XYZ # Change XYZ to the UID of your Pressure Bricklet
-
-# Get current pressure
-tinkerforge call pressure-bricklet $uid get-pressure
